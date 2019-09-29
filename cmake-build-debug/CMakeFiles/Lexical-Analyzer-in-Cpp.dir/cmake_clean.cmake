216cbffb9d1ed74fc7a41cfe4e5fe7aa696b80a1
Doetheman <doethegreat@gmail.com>
2019-09-29 17:38:02 -0400

    lexical analyzer

---
+++ cmake-build-debug/CMakeFiles/Lexical-Analyzer-in-Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lexical-Analyzer-in-Cpp.dir/lexicalanalyzer.cpp.obj"
+  "Lexical-Analyzer-in-Cpp.pdb"
+  "Lexical-Analyzer-in-Cpp.exe"
+  "Lexical-Analyzer-in-Cpp.exe.manifest"
+  "libLexical-Analyzer-in-Cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lexical-Analyzer-in-Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
